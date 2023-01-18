21635fa38b7a4dabe435be95cffc35d50aba9875
Sailesh Ranjan Mohanty <saileshranjanmohanty@gmail.com>
2023-01-18 23:19:37 +0530

    correction

--- _Functions_/Config.py
+++ _Functions_/Config.py
@@ -1,6 +1,4 @@
 import numpy as np
 
-h=8 #step size
+h=6 #step size
 
-Ec_min= 100 #Mev per fm^3 (To be set according to EOS for stability)
-Ec_max= 10000 #Mev per fm^3 (To be set according to EOS for stability)
--- _Functions_/Constants.py
+++ _Functions_/Constants.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 G= 6.6743e-11
-c= 3e+8
-M0= 1.989e+30
+c= 299792458
+M0= 1.98847e+30
+el = 1.60217662e-19
+hbar = 1.054571800e-34
+
 pi= np.pi
 
-Mev_per_fm_cube__to__Pa= 1.60218e+32 
+Mev_per_fm_cube__to__Pa= el*1e6/(1e-15)**3
 Pa__to__per_m_square= G/c**4
 Kg__to__m= G/c**2
